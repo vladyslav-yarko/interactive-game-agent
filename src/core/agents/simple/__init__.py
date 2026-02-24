29e3d7557f0fa958eac747e98ad6a03dfc2e6462
vladyslav-yarko <vladislavv44444@gmail.com>
2026-02-25 01:35:45 +0200

    feat(core): add simple agent

--- src/core/agents/simple/__init__.py
+++ src/core/agents/simple/__init__.py
@@ -1,1 +1,10 @@
 # Test agent used for working with simple graph
+
+
+from src.core.agents.local import LocalAgent
+from src.core.graphs.simple import simple_graph
+
+
+simple_agent = LocalAgent(
+    graph=simple_graph
+)
