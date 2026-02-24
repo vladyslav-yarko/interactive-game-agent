e604bf1e26e53debd8f726a8042a17e878c4c485
vladyslav-yarko <vladislavv44444@gmail.com>
2026-02-24 20:00:10 +0200

    feat(core): add BaseGraph

---
+++ src/core/graph/base.py
@@ -0,0 +1,29 @@
+from langgraph.graph import StateGraph
+from langgraph.graph.state import CompiledStateGraph
+from pydantic import BaseModel
+
+from src.core.graph.graph import Graph
+
+
+class BaseGraph(Graph):
+    def __init__(
+        self,
+        overall_state: BaseModel,
+        input_state: BaseModel,
+        output_state: BaseModel
+    ):
+        self.overall_state = overall_state
+        self.input_state = input_state
+        self.output_state = output_state
+        self.graph = StateGraph(
+            self.overall_state,
+            input_schema=self.input_state,
+            output_schema=self.output_state
+        )
+
+    def build(self, builder) -> None:
+        pass
+    
+    def compile(self) -> CompiledStateGraph:
+        graph = self.graph.compile()
+        return graph
